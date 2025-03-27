79cc5a1120048a057efe71137208da325f1beffb
timwakin <tim.akin@outlook.com>
2025-03-27 16:05:20 -0500

    adding debug files

--- obj/Debug/net8.0/TequilasRestaurant.AssemblyInfo.cs
+++ obj/Debug/net8.0/TequilasRestaurant.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TequilasRestaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214387dfa25e879ae8b3a89c5f74d0e8bd9dd246")]
 [assembly: System.Reflection.AssemblyProductAttribute("TequilasRestaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TequilasRestaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
