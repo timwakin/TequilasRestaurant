879f9fe81e2502e4bd2ecaffa93d3b7f7aeac294
timwakin <tim.akin@outlook.com>
2025-04-02 11:03:53 -0500

    added ingredient controller

---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TequilasRestaurant.Data;
+using TequilasRestaurant.Models;
+
+namespace TequilasRestaurant.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private Repository<Ingredient> ingredients; 
+
+        public IngredientController(ApplicationDbContext context)
+        {
+            ingredients = new Repository<Ingredient>(context); 
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await ingredients.GetAllAsync() );
+        }
+    }
+}
