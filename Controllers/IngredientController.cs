d58d9047a35f7c25f691d96382b5ce438fb3812c
timwakin <tim.akin@outlook.com>
2025-04-02 13:31:40 -0500

    committing changes pre-merge

---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TequilasRestaurant.Data;
+using TequilasRestaurant.Models;
+
+namespace TequilasRestaurant.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private Repository<Ingredient> ingredients; 
+
+        public IngredientController(ApplicationDbContext context)
+        {
+            ingredients = new Repository<Ingredient>(context); 
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await ingredients.GetAllAsync() );
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() {Includes = "ProductIngredients.Product" })); 
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TequilasRestaurant.Data;
+
+namespace TequilasRestaurant.Models
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected ApplicationDbContext _context { get; set; }
+        private DbSet<T> _dbSet { get; set; }
+        public Repository(ApplicationDbContext context) 
+        {
+            _context = context; 
+            _dbSet = context.Set<T>();
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id, QueryOptions<T> options)
+        {
+           IQueryable<T> query = _dbSet;
+            if (options.HasWhere)
+            {
+                query = query.Where(options.Where);
+            }
+            if (options.HasOrderBy)
+            {
+                query = query.OrderBy(options.OrderBy);
+            }
+            foreach (string include in options.GetIncludes())
+            {
+                query = query.Include(include);
+            }
+            var key = _context.Model.FindEntityType(typeof(T)).FindPrimaryKey().Properties.FirstOrDefault();
+            string primaryKeyName = key?.Name;
+            return await query.FirstOrDefaultAsync(e => EF.Property<int>(e, primaryKeyName) == id);
+
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
